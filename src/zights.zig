bc2456a2ac1dc3db2c4dd55573c7656aa2a93937
kcleal <clealk@cardiff.ac.uk>
2024-02-19 13:41:55 +0000

    Updated README.md

--- src/zights.zig
+++ src/zights.zig
@@ -1,14 +1,7 @@
-// const std = @import("std");
 
-pub const hts = @cImport({@cInclude("hts.h");});
-pub const hfile = @cImport({@cInclude("hfile.h");});
-pub const sam = @cImport({@cInclude("sam.h");});
+pub const zights = @cImport({
+        @cInclude("hts.h");
+        @cInclude("hfile.h");
+        @cInclude("sam.h");
+});
 
-// pub const zights = struct {
-//     const hts = @cImport({
-//         @cInclude("hts.h");
-//         @cInclude("hfile.h");
-//         @cInclude("sam.h");
-//     });
-
-// };
